18d98acb59d0b77aa5a2d07433a8c3d022b8832c
dalborgo <marco@dalborgo.it>
2023-11-16 12:55:02 +0100

    from lavoro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import { Button, createTheme } from '@mui/material'
-import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
+import React, { useEffect, useState } from 'react'
+import { Alert, Box, Button, createTheme, CssBaseline, Paper, ThemeProvider, Typography } from '@mui/material'
+import { makeStyles } from '@mui/styles'
 
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
 })
-const handleZpFunc = event => {
-  fetch(`http://localhost:5003/&zpfunc=${event.target.id}`, { mode: 'no-cors' }).then()
-}
+
+const useStyles = makeStyles(theme => ({
+  fullHeightPaper: {
+    height: '100vh', // Imposta l'altezza al 100% dell'area visibile
+  },
+  mainContainer: {
+    height: '100%', // Assicurati che il contenitore principale abbia un'altezza del 100%
+  },
+}))
 
 export default function App () {
+  const classes = useStyles()
+  const [isConnected, setIsConnected] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  
+  useEffect(() => {
+    setIsLoading(true)
+    checkConnection()
+  }, [])
+  
+  const checkConnection = () => {
+    fetch('http://localhost:3000/zoom/connect')
+      .then(response => response.json())
+      .then(data => {
+        setIsConnected(data.ok)
+        setIsLoading(false)
+      })
+      .catch(error => {
+        console.error('Errore nella verifica della connessione:', error)
+        setIsLoading(false)
+      })
+  }
+  
+  const handleZpFunc = event => {
+    fetch(`http://localhost:5003/&zpfunc=${event.target.id}`, { mode: 'no-cors' }).then()
+  }
+  
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline/>
-      <main>
-        <Button id="fnPlay" variant="contained" color="primary" onClick={handleZpFunc} size="small">
-          PLAY
-        </Button>
-      </main>
+      <Box className={classes.mainContainer}>
+        <Paper className={classes.fullHeightPaper}>
+          {
+            isConnected ? (
+              <Typography><Alert severity="success">Connesso!</Alert></Typography>
+            ) : (
+              <Typography>{
+                isLoading ?
+                  <Alert severity="info">Loading!</Alert>
+                  :
+                  <Alert severity="error">Connessione non riuscita!</Alert>
+              }
+              </Typography>
+            )
+          }
+          <Box p={2}>
+            <Button id="fnPlay" variant="contained" color="primary" onClick={handleZpFunc} size="small">
+              PLAY
+            </Button>
+          </Box>
+        </Paper>
+      </Box>
     </ThemeProvider>
   )
 }
