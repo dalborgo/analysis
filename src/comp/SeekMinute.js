3f21f0cafa5b4ebc666c797a7ead48193d387e30
Marco <marco@dalborgo.it>
2024-09-25 21:35:24 +0200

    first da casa

--- src/comp/SeekMinute.js
+++ src/comp/SeekMinute.js
@@ -8,7 +8,7 @@ import { Box, createTheme, IconButton, ThemeProvider, ToggleButton, ToggleButton
 function SeekMinute ({ goTime, period = 1, setFullMode, fullMode, halfTimeEnd, wyView, setWyView }) {
   const theme = useTheme()
   return (
-    <Box display="flex">
+    <Box>
       <ThemeProvider
         theme={createTheme({
           components: {
@@ -49,27 +49,30 @@ function SeekMinute ({ goTime, period = 1, setFullMode, fullMode, halfTimeEnd, w
             </ToggleButtonGroup>
           </Box>
         }
-        <Box display="flex" m={1} border={'1px solid #2A2929'} p={1} pt={0.5} pb={0.5}>
-          
-          <List dense
-                sx={{ bgcolor: 'background.paper', padding: 0 }}
-          >
-            {
-              Array.from({ length: 51 }).map((item, index) => {
-                return (<ListItem
-                  key={index}
-                  disablePadding
-                  onClick={() => goTime({ minute: index + 1, period })}
-                >
-                  <ListItemButton tabIndex={-1} style={{ textAlign: 'center', padding: 0, marginRight: -8 }}
-                                  sx={{ color: theme.palette.primary.main }}>
-                    <ListItemText primary={index + 1 + (fullMode ? 45 : 0)} sx={{ margin: 0, fontSize: 12 }}
-                                  disableTypography/>
-                  </ListItemButton>
-                </ListItem>)
-              })}
-          </List>&nbsp;&nbsp;
+        <Box display="flex">
+          <Box display="flex" m={1} border="1px solid #2A2929" p={1} pt={0.5} pb={0.5}>
+            
+            <List dense
+                  sx={{ padding: 0 }}
+            >
+              {
+                Array.from({ length: 51 }).map((item, index) => {
+                  return (<ListItem
+                    key={index}
+                    disablePadding
+                    onClick={() => goTime({ minute: index + 1, period })}
+                  >
+                    <ListItemButton tabIndex={-1} style={{ textAlign: 'center', padding: 0, marginRight: -8 }}
+                                    sx={{ color: theme.palette.primary.main }}>
+                      <ListItemText primary={index + 1 + (fullMode ? 45 : 0)} sx={{ margin: 0, fontSize: 12 }}
+                                    disableTypography/>
+                    </ListItemButton>
+                  </ListItem>)
+                })}
+            </List>&nbsp;&nbsp;
+          </Box>
         </Box>
+      
       </ThemeProvider>
     </Box>
   )
