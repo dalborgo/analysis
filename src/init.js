ae3d10336232f47d862eb4af03f351a235833030
Marco <marco@dalborgo.it>
2024-10-26 18:47:40 +0200

    first da casa

--- src/init.js
+++ src/init.js
@@ -1,6 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production'
-const REACT_APP = isProd ? 'REACT_APP' : 'REACT_APP_DEV'
-const BACKEND_PORT = process.env[`${REACT_APP}_BACKEND_PORT`]
+const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT;
 
 export const envConfig = {
   BACKEND_PORT,
