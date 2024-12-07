072e02c88c1a4c445997dec500498297f93986f2
Marco <marco@dalborgo.it>
2024-12-07 11:50:10 +0100

    first da casa

--- src/App.js
+++ src/App.js
@@ -495,9 +495,7 @@ export default function App ({ halfTime, initTime = 0, homeDir = false }) {
             const direction = document.getElementById('direction')
             const time = convertMilli(parseInt(result), halfTimeEnd, initTimeEnd, fullMode)
             if (elemEff) {elemEff.textContent = time.effectiveLong === 'NaN:NaN' ? '--:--' : time.effectiveLong}
-            if (elemLong) {
-              elemLong.textContent = time.long === 'NaN:NaN:NaN' ? '--:--' : time.long
-            }
+            if (elemLong) {elemLong.textContent = time.long === 'NaN:NaN:NaN' ? '--:--' : time.long}
             elemShort.textContent = time.short === 'NaN′' ? '--' : time.short
             milliBox.value = result
             if (fractionElem && !time.short.startsWith('0')) {
