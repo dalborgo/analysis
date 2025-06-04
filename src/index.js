435a76f376e187028201fd3ced4b9bf5d9b79db7
dalborgo <marco@dalborgo.it>
2025-06-04 11:15:53 +0200

    da lavoro

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+
 const savedHalfTime = localStorage.getItem('halfTimeEnd')
 const savedInitTime = localStorage.getItem('initTimeEnd')
 const savedHomeDir = localStorage.getItem('homeDirEnd')
-const halfTime = savedHalfTime ? parseInt(savedHalfTime, 0) : null
-const initTime = savedInitTime ? parseInt(savedInitTime, 0) : null
-const homeDir = savedHomeDir ? Boolean(parseInt(savedHomeDir, 0)) : false
+const halfTime = savedHalfTime ? parseInt(savedHalfTime, 10) : null
+const initTime = savedInitTime ? parseInt(savedInitTime, 10) : null
+const homeDir = savedHomeDir ? Boolean(parseInt(savedHomeDir, 10)) : false
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App halfTime={halfTime} initTime={initTime} homeDir={homeDir}/>)
